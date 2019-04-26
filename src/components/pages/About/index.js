501215eea921cc6f22fd84ebe4d31b8a4c50c4e1
Abderrhim Soubai <gx.coder@gmail.com>
2019-04-26 01:13:53 +0100

    Add some description in the About page

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
     return (
       <div className="text-black">
-        <h1>About</h1>
+        <div className="mt-10">
+          <h1>About</h1>
+          <p className="mt-5">
+            Pentium radio (formerly Typehints) is a Moroccan podcast (Moroccan
+            dialect aka Darija) hosted by{" "}
+            <b>
+              {" "}
+              <a href="https://twitter.com/Ginix_Labs">Hussein Jeffery</a>
+            </b>{" "}
+            and
+            <b>
+              {" "}
+              <a href="https://twitter.com/Ginix_Labs">Abderrahim Soubai</a>
+            </b>{" "}
+            interested in web development and software engineering culture.
+          </p>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default About
+export default About;
