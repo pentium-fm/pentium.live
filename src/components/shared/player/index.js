617b842075eaaa049fece75952a88b3ed9e68d55
Jeffery Hussin <jefferytutorials@gmail.com>
2019-07-10 20:31:18 +0100

    player audio auto play on change, done

--- src/components/shared/player/index.js
+++ src/components/shared/player/index.js
@@ -5,21 +5,33 @@ import {
 } from "../../../store/reducers";
 
 class Player extends Component {
-  componentDidUpdate = () => {
-    console.log("hey")
-  }
+  componentWillReceiveProps(nextProps) {
+    let { current } = nextProps;
 
+    if (current) {
+      if (this.audio) {
+        this.audio.pause();
+        this.audio.load();
+        this.audio.play();
+      }
+    }
+  }
+  
   render() {
     let { audio } = this.props.current;
 
     return (
-      <div className="flex fixed bottom left">
-        <audio
-          controls
-          ref={audio => (this.audio = audio)}
-        >
-          <source src={audio} type="audio/mpeg" />
-        </audio>
+      <div className="flex fixed w-full p-5 bg-white shadow bottom-0 left-0">
+        {audio ? (
+          <audio
+            className="hidden"
+            autoPlay
+            controls
+            ref={audio => (this.audio = audio)}
+          >
+            <source src={audio} type="audio/mpeg" />
+          </audio>
+        ) : ""}
       </div>
     )
   }
