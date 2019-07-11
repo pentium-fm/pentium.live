634aad425cc280e14ba40c12bd96d2816450d92a
Abderrhim Soubai <gx.coder@gmail.com>
2019-07-12 00:34:22 +0100

    feat: add question btn

--- src/components/shared/navbar/index.js
+++ src/components/shared/navbar/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import Sidebar from './sidebar';
+import Sidebar from "./sidebar";
 
 class Navbar extends Component {
   constructor(props) {
@@ -10,11 +10,11 @@ class Navbar extends Component {
 
     this.state = {
       show: false
-    }
+    };
   }
 
   render() {
-    let {show} = this.state;
+    let { show } = this.state;
 
     return (
       <div className="flex flex-no-wrap content-start items-center flex-grow">
@@ -58,6 +58,13 @@ class Navbar extends Component {
             </div>
           </div>
           <div className="flex flex-wrap w-full items-center justify-end">
+            <a href="https://forms.gle/DN2N53cPWKHGaDRZ7" target="_blank">
+              {" "}
+              <button className="bg-transparent border border-white text-white hover:border-black hover:text-black transition rounded-full w-full text-sm py-2 px-2">
+                <span className="text ml-2"> Ask a question </span>
+              </button>{" "}
+            </a>
+
             <div className="flex flex-wrap items-center content-start justify-center px-4 py-3 lg:p-4">
               <span className="text-2xl text-center cursor-pointer w-full">
                 <FontAwesomeIcon
@@ -66,7 +73,7 @@ class Navbar extends Component {
                   onClick={() => {
                     this.setState({
                       show: !show
-                    })
+                    });
                   }}
                 />
               </span>
